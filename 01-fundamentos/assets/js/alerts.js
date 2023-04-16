d753a66bc46c85c6e55eed7426ff03587fcd27c5
louhenry <louhenry@gmail.com>
2023-04-16 10:21:44 -0600

    first commit

---
+++ 01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,7 @@
+
+
+// alert('Hola Mundo');
+// prompt('Agregue aca su nombre');
+
+const seleccion = confirm('Esta seguro?');
+console.log(seleccion);
---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,21 @@
+//  console.log('Hola Mundo');
+
+let a = 10,
+    b = 20,
+    c = 'Hola',
+    d = 'Mundo',
+    x = a+b;
+
+// console.log('a', a)
+// console.log('b', b)
+// console.log('c', c)
+
+
+// console.log({a})
+// console.log({b})
+// console.log({c})
+
+// console.table({ a, b, c, d, x });
+
+
+var miNombre = 'Luis';
